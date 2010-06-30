d1c47d91f286c9d7e685a7fe315ced9a80e3aa90
btimby <btimby@a9284870-ed97-e9da-a84a-3e33291ac869>
2010-06-30 15:17:40 +0000

    - working test suite.

--- src/tests.py
+++ src/tests.py
@@ -6,11 +6,22 @@ from faxage import fax, provision
 LOCAL_SERVER_ADDR = 'localhost'
 LOCAL_SERVER_PORT = 8008
 FAX_NO = '555-555-5555'
+TEST_PDF_FILE = 'test.pdf'
+TEST_COMPANY = 'company'
+TEST_USERNAME = 'username'
+TEST_PASSWORD = 'password'
 
 class TestHTTPHandler(BaseHTTPRequestHandler):
+    def log_message(*args):
+        pass
+
+    def send_faxage_error(self, errline):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(errline)
+
     def do_POST(self):
         try:
-            print 'parsing form...'
             form = cgi.FieldStorage(
                 fp          = self.rfile,
                 headers     = self.headers,
@@ -19,21 +30,25 @@ class TestHTTPHandler(BaseHTTPRequestHandler):
                     'CONTENT_TYPE':         self.headers['Content-Type'],
                 }
             )
+            #authenticate user:
+            if form.getvalue('company') != TEST_COMPANY or \
+               form.getvalue('username') != TEST_USERNAME or \
+               form.getvalue('password') != TEST_PASSWORD:
+                return self.send_faxage_error('ERR02: Login incorrect')
+            #map request to an operation handler
             operation = form.getvalue('operation')
             handler = getattr(self, 'handle_' + operation, None)
             if handler and callable(handler):
                 handler(form)
         except Exception, e:
-            print 'error:', str(e)
             self.send_error(500, str(e))
 
     def handle_sendfax(self, form):
-        print 'hander called...'
+        if self.path != '/httpsfax.php':
+            return self.send_error(404, 'File Not Found')
         self.send_response(200)
         self.end_headers()
-        for key in form.keys():
-            value = form.getvalue(key)
-            self.wfile.write('%s=%s\n' % (name, value))
+        self.wfile.write('JOBID: 1234')
 
 class TestHTTPServer(HTTPServer, Thread):
     def __init__(self, address):
@@ -46,22 +61,42 @@ class TestHTTPServer(HTTPServer, Thread):
 
 class TestFaxClient(unittest.TestCase):
     def setUp(self):
-        self.client = fax.FaxClient('company', 'username', 'password', use_ssl=False, host=LOCAL_SERVER_ADDR, port=LOCAL_SERVER_PORT)
+        self.client = fax.FaxClient(
+            TEST_COMPANY,
+            TEST_USERNAME,
+            TEST_PASSWORD,
+            use_ssl     = False,
+            host        = LOCAL_SERVER_ADDR,
+            port        = LOCAL_SERVER_PORT
+        )
+
+    def testInvalidCredentials(self):
+        badclient = fax.FaxClient(
+            'badcompany',
+            'badusername',
+            'badpassword',
+            use_ssl     = False,
+            host        = LOCAL_SERVER_ADDR,
+            port        = LOCAL_SERVER_PORT
+        )
+        try:
+            badclient.send_fax(FAX_NO, TEST_PDF_FILE)
+        except Exception, e:
+            self.assertEqual(str(e), 'ERR02: Login incorrect')
+        else:
+            self.fail('Invalid credentials did not result in an error.')
 
     def testSendFax(self):
-        self.client.send_fax(FAX_NO, 'test.pdf')
+        self.client.send_fax(FAX_NO, TEST_PDF_FILE)
 
     def testSendFaxWithFileObj(self):
-        with file('test.pdf', 'r') as faxfile:
+        with file(TEST_PDF_FILE, 'r') as faxfile:
             self.client.send_fax(FAX_NO, 'fax1.pdf', file_obj=faxfile)
 
 if __name__ == '__main__':
-    print 'starting webserver...'
     server = TestHTTPServer((LOCAL_SERVER_ADDR, LOCAL_SERVER_PORT))
     try:
         unittest.main()
-    except SystemExit:
-        pass
-    print 'stopping webserver...'
-    time.sleep(1) # not sure why this is needed, but it is...
-    server.shutdown()
+    finally:
+        time.sleep(1) # not sure why this is needed, but it is...
+        server.shutdown()
